acd349043d953678137424f2dba6bd5fbb957d12
נתנאל בשן <nbashan@g.jct.ac.il>
2022-11-26 19:29:03 +0200

    generate graphql entities for contact running nest g resource contact

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { join } from 'path';
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
     }),
+    ContactModule,
   ],
 })
 export class AppModule {}
